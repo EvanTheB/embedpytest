d19069a62ffcfb5f31f537b815121bfd9d24d063
EvanTheB <evanbenn@gmail.com>
2017-10-16 19:32:19 +0800

    repl can call back to c

--- test1.c
+++ test1.c
@@ -1,6 +1,16 @@
+#include "stdio.h"
+
 extern void repl(void);
+
+int value = 0;
+
+void print_value(void)
+{
+    printf("%d\n", value);
+}
 
 int main(void)
 {
+    print_value();
     repl();
 }
--- test1.py
+++ test1.py
@@ -2,12 +2,29 @@ import cffi
 
 ffibuilder = cffi.FFI()
 
+# the API exported by the python stuff
 ffibuilder.embedding_api("""
     void repl(void);
 """)
 
+# declare things that the python code wants to use?
+# wrappers are generated
+ffibuilder.cdef("""
+    extern int value;
+    void print_value(void);
+""")
+
+# verbatim copied to the output c
+# need both this and cdef
+ffibuilder.set_source("test1_gen", """
+    extern int value;
+    void print_value(void);
+""")
+
+# init time once only code, called on first api function call
+# the cdef and set_source stuff is in lib
 ffibuilder.embedding_init_code("""
-    from test1_gen import ffi
+    from test1_gen import ffi, lib
     import code
 
     @ffi.def_extern()
@@ -15,6 +32,17 @@ ffibuilder.embedding_init_code("""
         code.InteractiveConsole(locals=globals()).interact()
 """)
 
-ffibuilder.set_source("test1_gen", "")
-
 ffibuilder.compile(verbose=True)
+
+
+# >>> from cffi import FFI
+# >>> ffi = FFI()
+# >>> ffi.cdef("""
+# ...     int printf(const char *format, ...);   // copy-pasted from the man page
+# ... """)
+# >>> C = ffi.dlopen(None)                     # loads the entire C namespace
+# >>> arg = ffi.new("char[]", "world")         # equivalent to C code: char arg[] = "world";
+# >>> C.printf("hi there, %s.\n", arg)         # call printf
+# hi there, world.
+# 17                                           # this is the return value
+# >>>
