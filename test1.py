1959989749f9b609dd1b21624abf12b4c782e66a
EvanTheB <evanbenn@gmail.com>
2017-10-16 19:36:49 +0800

    cleaner comments

--- test1.py
+++ test1.py
@@ -2,13 +2,14 @@ import cffi
 
 ffibuilder = cffi.FFI()
 
-# the API exported by the python stuff
+# the API exported by the python code
 ffibuilder.embedding_api("""
     void repl(void);
 """)
 
-# declare things that the python code wants to use?
+# declare things that the python code wants to use
 # wrappers are generated
+# without this init_code lib has nothing
 ffibuilder.cdef("""
     extern int value;
     void print_value(void);
@@ -16,6 +17,7 @@ ffibuilder.cdef("""
 
 # verbatim copied to the output c
 # need both this and cdef
+# without this the c file wont compile (cdef wrappers refer to undeclared objects)
 ffibuilder.set_source("test1_gen", """
     extern int value;
     void print_value(void);
@@ -33,16 +35,3 @@ ffibuilder.embedding_init_code("""
 """)
 
 ffibuilder.compile(verbose=True)
-
-
-# >>> from cffi import FFI
-# >>> ffi = FFI()
-# >>> ffi.cdef("""
-# ...     int printf(const char *format, ...);   // copy-pasted from the man page
-# ... """)
-# >>> C = ffi.dlopen(None)                     # loads the entire C namespace
-# >>> arg = ffi.new("char[]", "world")         # equivalent to C code: char arg[] = "world";
-# >>> C.printf("hi there, %s.\n", arg)         # call printf
-# hi there, world.
-# 17                                           # this is the return value
-# >>>
